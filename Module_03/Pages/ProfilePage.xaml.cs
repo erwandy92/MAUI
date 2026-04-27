2a3b2ea98ae1a236d714550161bc170f939ce715
Erwandy Lim <erwandy.dev@gmail.com>
2026-04-27 14:31:00 +0800

    Shell: Added Pages and setup Flyout Shell

---
+++ Module_03/Pages/ProfilePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Module_03.Pages;
+
+public partial class ProfilePage : ContentPage
+{
+	public ProfilePage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Module_03/Pages/ShopPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Module_03.Pages;
+
+public partial class ShopPage : ContentPage
+{
+	public ShopPage()
+	{
+		InitializeComponent();
+	}
+}
