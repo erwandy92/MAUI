c5139124cd9ff5b7148a06f17c9fd8a427c6cdd6
Erwandy Lim <erwandy.dev@gmail.com>
2026-04-27 11:44:56 +0800

    Modify Layout, Added new ContentPage.Resources, Added button to get OS Version

--- Module_02/MainPage.xaml.cs
+++ Module_02/MainPage.xaml.cs
@@ -2,23 +2,14 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+        private void btn_GetOSVersion(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            lblOSVersion.Text = $"Platform: {DeviceInfo.Current.Platform}, Version: {DeviceInfo.Current.VersionString}";
         }
     }
 }
