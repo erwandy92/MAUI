5229662ea588419a614c92d453c22242b979e6a7
Erwandy Lim <erwandy.dev@gmail.com>
2026-04-28 10:14:20 +0800

    Layout: StackLayout Orientation

--- Module_04/Views/StackLayoutPage.xaml.cs
+++ Module_04/Views/StackLayoutPage.xaml.cs
@@ -6,4 +6,13 @@ public partial class StackLayoutPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void btnChangeLayout_Clicked(object sender, EventArgs e)
+    {
+        stack1.Orientation = StackOrientation.Vertical;
+        lblPizza.HorizontalOptions = LayoutOptions.Start;
+        lblSushi.HorizontalOptions = LayoutOptions.End;
+        lblBurger.HorizontalOptions = LayoutOptions.Center;
+        lblPasta.HorizontalOptions = LayoutOptions.Fill;
+    }
 }
