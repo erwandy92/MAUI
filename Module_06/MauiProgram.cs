cb4aca618cb879530a0440df551d3f9db900f2ec
Erwandy Lim <erwandy.dev@gmail.com>
2026-04-28 15:05:11 +0800

    Button: Event

--- Module_06/MauiProgram.cs
+++ Module_06/MauiProgram.cs
@@ -13,6 +13,9 @@ namespace Module_06
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Font Awesome 6 Free-Regular-400.otf", "FA6FR");
+                    fonts.AddFont("Font Awesome 6 Free-Solid-900.otf", "FA6FS");
+                    fonts.AddFont("Font Awesome 6 Brands-Regular-400.otf", "FA6BR");
                 });
 
 #if DEBUG
