1192e1d1afc7f168701b218635dc26fe6de2c8bf
Erwandy Lim <erwandy.dev@gmail.com>
2026-04-28 09:34:00 +0800

    Layout: VStackLayout - Applied Padding & Margin Thickness

--- Module_04/Views/VStackLayoutPage.xaml.cs
+++ Module_04/Views/VStackLayoutPage.xaml.cs
@@ -6,4 +6,16 @@ public partial class VStackLayoutPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void btnTacos_Clicked(object sender, EventArgs e)
+    {
+        btnChangeMargin.TextColor = Colors.Black;
+        btnChangeMargin.Margin = new Thickness(10, 20, 30, 40);
+    }
+
+    private void btnRamen_Clicked(object sender, EventArgs e)
+    {
+        btnChangePadding.TextColor = Colors.Black;
+        btnChangePadding.Padding = new Thickness(20);
+    }
 }
