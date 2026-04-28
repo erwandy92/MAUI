8b0e98eae3da448b76a8d52cac01e96f4615b55c
Erwandy Lim <erwandy.dev@gmail.com>
2026-04-28 11:29:31 +0800

    Font: Added custom Fonts and FontAwesome Icon by Unicode

--- Module_05/MauiProgram.cs
+++ Module_05/MauiProgram.cs
@@ -11,8 +11,21 @@ namespace Module_05
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
+                    //Default
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+
+                    //Added Fonts
+                    fonts.AddFont("NotoSerifTC-ExtraBold.ttf", "SerifEB");
+                    fonts.AddFont("NotoSerifTC-Black.ttf", "SerifBk");
+                    fonts.AddFont("NotoSerifTC-Regular.ttf", "SerifR");
+                    fonts.AddFont("ZenMaruGothic-Black.ttf", "ZMGB");
+                    fonts.AddFont("ZenMaruGothic-Regular.ttf", "ZMGR");
+                    fonts.AddFont("RampartOne-Regular.ttf", "ROR");
+                    fonts.AddFont("Font Awesome 6 Free-Regular-400.otf", "FA6FR");
+                    fonts.AddFont("Font Awesome 6 Free-Solid-900.otf", "FA6FS");
+                    fonts.AddFont("Font Awesome 6 Brands-Regular-400.otf", "FA6BR");
+
                 });
 
 #if DEBUG
