48b777751fd521779fca9f83a104fc443e42fc79
Erwandy Lim <erwandy.dev@gmail.com>
2026-04-29 09:58:03 +0800

    Initial Commit

---
+++ Module_07_02/Platforms/Android/MainActivity.cs
@@ -0,0 +1,11 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace Module_07_02
+{
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    public class MainActivity : MauiAppCompatActivity
+    {
+    }
+}
---
+++ Module_07_02/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Module_07_02
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
