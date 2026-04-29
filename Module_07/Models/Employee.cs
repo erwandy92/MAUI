27ae2362a4692c0b6c093fd55247324799659c2d
Erwandy Lim <erwandy.dev@gmail.com>
2026-04-29 09:33:46 +0800

    Binding Source from Model: Basic Binding

---
+++ Module_07/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod071.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime HireDate { get; set; }
+    public decimal Salary { get; set; }
+}
+
