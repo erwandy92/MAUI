a02f5bdb7721b10c0b494b967d64e3ceea7de633
Erwandy Lim <erwandy.dev@gmail.com>
2026-04-29 15:06:01 +0800

    MVVMS: Singleton vs Transient in Services

--- Module_09/MauiProgram.cs
+++ Module_09/MauiProgram.cs
@@ -22,8 +22,21 @@ namespace Module_09
 #endif
 
             //Added DI Singleton - ViewModel and View
-            builder.Services.AddSingleton<TimeViewModel>();
-            builder.Services.AddSingleton<TimeView>();
+            //Scenario 1 - The data and view will persist when it's being recalled
+            builder.Services.AddSingleton<TimeViewModel>(); //ViewModel (Data Handling part)
+            builder.Services.AddSingleton<TimeView>(); //View
+
+            //Scenario 2 - The data and view will be recreated when it's being recalled
+            //builder.Services.AddTransient<TimeViewModel>();
+            //builder.Services.AddTransient<TimeView>();
+
+            //Scenario 3 - The data will persist, but the view will be recreated when it's being recalled
+            //builder.Services.AddSingleton<TimeViewModel>();
+            //builder.Services.AddTransient<TimeView>();
+
+            //Scenario 4 - The data and view will persist when it's being recalled (due to the View keeps showing the same one upon Singleton)
+            //builder.Services.AddTransient<TimeViewModel>();
+            //builder.Services.AddSingleton<TimeView>();
 
             return builder.Build();
         }
