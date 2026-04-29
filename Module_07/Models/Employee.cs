082320481165b8babbd2c8b22a4c7d8fb3481bcd
Erwandy Lim <erwandy.dev@gmail.com>
2026-04-29 09:54:02 +0800

    Binding Source from Model: Added INotifyPropertyChanged - to get notified when the data is changed upon binding

--- Module_07/Models/Employee.cs
+++ Module_07/Models/Employee.cs
@@ -1,16 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Mod071.Models;
 
-public class Employee
+public class Employee: INotifyPropertyChanged
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public DateTime HireDate { get; set; }
-    public decimal Salary { get; set; }
+    private int id;
+    private string name;
+    private DateTime hireDate;
+    private decimal salary;
+
+    public int Id
+    {
+        get => id;
+        set
+        {
+            if (id != value)
+            {
+                id = value;
+                OnPropertyChanged(nameof(Id));
+            }
+        }
+    }
+
+    public string Name
+    {
+        get => name;
+        set
+        {
+            if (name != value)
+            {
+                name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+    }
+
+    public DateTime HireDate
+    {
+        get => hireDate;
+        set
+        {
+            if (hireDate != value)
+            {
+                hireDate = value;
+                OnPropertyChanged(nameof(HireDate));
+            }
+        }
+    }
+
+    public decimal Salary
+    {
+        get => salary;
+        set
+        {
+            if (salary != value)
+            {
+                salary = value;
+                OnPropertyChanged(nameof(Salary));
+            }
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public void OnPropertyChanged(string propertyName) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
 }
 
