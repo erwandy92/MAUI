67beaf401da2d55c1f0535f3a8cb80f055eb0d5e
Erwandy Lim <erwandy.dev@gmail.com>
2026-04-29 12:01:13 +0800

    MVVM: Added PetViewModel

---
+++ Module_08/ViewModels/PetViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Module_08.Models;
+
+namespace Module_08.ViewModels;
+
+public class PetViewModel : ViewModelBase
+{
+    private PetModel _pet = new PetModel();
+    public PetModel Pet
+    {
+        get => _pet;
+        set
+        {
+            if (_pet != value)
+            {
+                _pet = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public PetViewModel()
+    {
+        Title = "寵物資訊";
+    }
+
+}
